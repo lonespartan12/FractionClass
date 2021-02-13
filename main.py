3dcb3a79d4735f73e32ae2fe49d698221e3f2c00
Christian Mudd <lonespartan12@hotmail.com>
2021-02-13 18:04:53 +0000

    Complete Fraction Class program as of Ch.1.13.2

--- main.py
+++ main.py
@@ -1,48 +1,37 @@
-class Fraction:
-    def __init__(self, top, bottom):
-        self.num = top
-        self.den = bottom
-
-    def show(self):
-      print(self.num,"/",self.den)
-    
-    def __str__(self):
-      return str(self.num)+"/"+str(self.den)
-      
-    
-    
-    def __add__(self,otherfraction):
-      newnum = self.num*otherfraction.den + self.den*otherfraction.num
-      newden = self.den*otherfraction.den
-      common = gcd(newnum,newden)
-      return Fraction(newnum//common,newden//common)
-
-    def __eq__(self, other):
-      firstnum = self.num * other.den
-      secondnum = other.num * self.den
-      return firstnum == secondnum
-
 def gcd(m,n):
-      while m%n != 0:
+    while m%n != 0:
         oldm = m
         oldn = n
+
         m = oldn
         n = oldm%oldn
-      return n
+    return n
 
-myf = Fraction(3,5)
-print(myf)
+class Fraction:
+     def __init__(self,top,bottom):
+         self.num = top
+         self.den = bottom
 
-myf.show()
+     def __str__(self):
+         return str(self.num)+"/"+str(self.den)
 
-print("I ate ", myf, "of the pizza")
+     def show(self):
+         print(self.num,"/",self.den)
 
-myf.__str__()
+     def __add__(self,otherfraction):
+         newnum = self.num*otherfraction.den + \
+                      self.den*otherfraction.num
+         newden = self.den * otherfraction.den
+         common = gcd(newnum,newden)
+         return Fraction(newnum//common,newden//common)
 
-str(myf)
+     def __eq__(self, other):
+         firstnum = self.num * other.den
+         secondnum = other.num * self.den
 
-f1 = Fraction(1,4)
-f2 = Fraction(1,2)
-f3 = f1+f2
-print(f3)
-print(f1 == f2)
+         return firstnum == secondnum
+
+x = Fraction(1,2)
+y = Fraction(2,3)
+print(x+y)
+print(x == y)
